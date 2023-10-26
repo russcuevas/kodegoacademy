112e8100ef716436b906a5fbe48663aa8cd14ad2
russcuevas <russelcuevas0@gmail.com>
2023-10-27 01:18:52 +0800

    hello

--- resources/views/admin/Layout/_Sidebar.blade.php
+++ resources/views/admin/Layout/_Sidebar.blade.php
@@ -2,32 +2,32 @@
     <ul class="nav flex-column">
         <li class="nav-item">
             <a class="nav-link {{ Request::route()->getName() == 'dashboardpage' ? 'active' : '' }}" href="{{ route('dashboardpage') }}">
-                <i class="fa-solid fa-house"><span> Dashboard</span></i>
+                <i class="fa-solid fa-house"></i><span style="font-weight: bold"> Dashboard </span>
             </a>
         </li>
         <li class="nav-item">
             <a class="nav-link {{ Request::route()->getName() == 'userspage' ? 'active' : '' }}" href="{{ route('userspage') }}">
-                <i class="fa-solid fa-user"><span> Users</span></i>
+                <i class="fa-solid fa-user"></i><span style="font-weight: bold"> Users </span>
             </a>
         </li>
         <li class="nav-item">
             <a class="nav-link {{ Request::route()->getName() == 'instructorspage' ? 'active' : '' }}" href="{{ route('instructorspage') }}">
-                <i class="fa-solid fa-person-chalkboard"><span> Instructors</span></i>
+                <i class="fa-solid fa-person-chalkboard"></i><span style="font-weight: bold"> Instructors </span>
             </a>
         </li>
         <li class="nav-item">
             <a class="nav-link {{ Request::route()->getName() === 'coursepage' ? 'active' : '' }}" href="{{ route('coursepage') }}">
-                <i class="fa-solid fa-code"><span> Course</span></i>
+                <i class="fa-solid fa-code"></i><span style="font-weight: bold"> Course </span>
             </a>
         </li>
         <li class="nav-item">
             <a class="nav-link" href="{{ route('homepage') }}">
-                <i class="fa-brands fa-chrome"><span> Page</span></i>
+                <i class="fa-brands fa-chrome"></i><span style="font-weight: bold"> Page </span>
             </a>
         </li>
         <li class="nav-item">
             <a class="nav-link" href="#">
-                <i class="fa-solid fa-gear"><span> Settings</span></i>
+                <i class="fa-solid fa-gear"></i><span style="font-weight: bold"> Settings </span>
             </a>
         </li>
     </ul>
