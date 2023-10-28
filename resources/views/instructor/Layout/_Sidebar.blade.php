eaf0fa3e70d76d3d54c97e0308b9b47acb78dae4
russcuevas <russelcuevas0@gmail.com>
2023-10-28 09:53:33 +0800

    hello

--- resources/views/instructor/Layout/_Sidebar.blade.php
+++ resources/views/instructor/Layout/_Sidebar.blade.php
@@ -2,22 +2,22 @@
     <ul class="nav flex-column">
         <li class="nav-item">
             <a class="nav-link {{ Request::route()->getName() == 'instructordb' ? 'active' : '' }}" href="{{ route('instructordb') }}">
-                <i class="fa-solid fa-house"><span> Dashboard</span></i>
+                <i class="fa-solid fa-house"></i><span style="font-weight: 900"> Dashboard</span>
             </a>
         </li>
         <li class="nav-item">
             <a class="nav-link {{ Request::route()->getName() == 'enrollpage' ? 'active' : '' }}"  href="{{ route('enrollpage') }}">
-                <i class="fa-solid fa-code"><span> Enrolled</span></i>
+                <i class="fa-solid fa-code"></i><span style="font-weight: 900"> Enrolled</span>
             </a>
         </li>
         <li class="nav-item">
             <a class="nav-link" href="/">
-                <i class="fa-brands fa-chrome"><span> Page</span></i>
+                <i class="fa-brands fa-chrome"></i><span style="font-weight: 900"> Page</span>
             </a>
         </li>
         <li class="nav-item">
             <a class="nav-link" href="#">
-                <i class="fa-solid fa-gear"><span> Settings</span></i>
+                <i class="fa-solid fa-gear"></i><span style="font-weight: 900"> Settings</span>
             </a>
         </li>
     </ul>
