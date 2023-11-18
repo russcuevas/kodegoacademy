b471af39cc946ffe6c504b891497929b5d27ca96
russcuevas <russelcuevas0@gmail.com>
2023-11-19 00:37:21 +0800

    hello

--- resources/views/page/reset_password.blade.php
+++ resources/views/page/reset_password.blade.php
@@ -5,20 +5,62 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="{{ asset('page/css/forgot_password.css')}}">
+    <style>
+        body {
+            font-family: Arial, sans-serif;
+            background-color: #f0f2f5;
+            margin: 0;
+            padding: 0;
+        }
+
+        .container {
+            max-width: 600px;
+            margin: 50px auto;
+            background-color: #ffffff;
+            padding: 20px;
+            box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
+            border-radius: 8px;
+        }
+
+        h1 {
+            color: #212529;
+        }
+
+        p {
+            line-height: 1.6;
+            color: #333;
+        }
+
+        a {
+            color: #1877f2;
+            text-decoration: none;
+        }
+
+        a:hover {
+            text-decoration: underline;
+        }
+
+        .reset-button {
+            display: inline-block;
+            padding: 10px 20px;
+            background-color: #9fef00;
+            color: #ffffff;
+            text-decoration: none;
+            border-radius: 5px;
+        }
+    </style>
     <title>Reset Password</title>
 </head>
 
 <body>
-    <p>Hey Mr./Ms. {{ $name }},</p>
-
-    <p>You are receiving this email because we received a password reset request for your account.</p>
-
-    <p><strong>Reset your password:</strong></p>
-
-    <p><a href="{{ $resetLink }}">Reset Password</a></p>
-
-    <p>If you did not request a password reset, no further action is required.</p>
+    <div class="container">
+        <h1>Reset Your Password</h1>
+        <p>Hey Mr./Ms. {{ $name }},</p>
+        <p>You are receiving this email because we received a password reset request for your account.</p>
+        <p><strong>Reset your password:</strong></p>
+        <p><a class="reset-button" href="{{ $resetLink }}">Reset Password</a></p>
+        <p>If you did not request a password reset, no further action is required.</p>
+    </div>
 </body>
 
 </html>
