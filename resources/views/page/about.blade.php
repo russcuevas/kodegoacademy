850b4c5b773fd9bed8c4f6118e80746a767dcc91
russcuevas <russelcuevas0@gmail.com>
2023-12-02 20:24:21 +0800

    hello

--- resources/views/page/about.blade.php
+++ resources/views/page/about.blade.php
@@ -8,20 +8,16 @@
             <div class="col-lg-6 text-left">
                 <p class="text-white">KodeGo gives individuals,
                 businesses and universities the tools they need to
-                continuously improve their programming skills.
-                KodeGo gives individuals,
-                businesses and universities the tools they need to
                 continuously improve their programming skills.</p>
-                <p class="text-white">KodeGo gives individuals,
-                businesses and universities the tools they need to
-                continuously improve their programming skills.
-                KodeGo gives individuals,
-                businesses and universities the tools they need to
-                continuously improve their programming skills.</p>
+                <p class="text-white"> <span style="font-size: 20px; font-weight: 900; color: #9fef00;">- Mission -</span> <br>
+                At Kodego Academy, our mission is to facilitate a seamless journey for individuals seeking proficiency in programming languages. We are dedicated to providing an unparalleled educational experience that equips learners with the skills and knowledge needed to master coding languages. Our commitment lies in demystifying the world of programming, making it accessible to all who aspire to embark on this transformative journey. Through a dynamic curriculum, hands-on projects, and a supportive community, we empower our students to excel in the art and science of coding, fostering a passion for innovation and problem-solving.                </p>
+                <p class="text-white"> <span style="font-size: 20px; font-weight: 900; color: #9fef00;">- Vision -</span> <br>
+                Our vision at Kodego Academy is to be the foremost destination for individuals aspiring to master programming languages. We envision a future where our graduates are not only proficient coders but also influential contributors to the ever-evolving tech landscape. By staying at the forefront of technological advancements, continuously refining our curriculum, and fostering a culture of continuous learning, we aim to be the driving force behind a global community of skilled programmers who shape the future through their expertise and creativity
+                </p>
             </div>
 
             <div class="col-lg-6 text-center">
-                <img src="{{ asset('page/images/navlogo.png') }}" alt="Logo" class="img-fluid flip" id="autoFlipImage">
+                <img style="margin-top: 200px" src="{{ asset('page/images/navlogo.png') }}" alt="Logo" class="img-fluid flip" id="autoFlipImage">
             </div>
 
             </div>
