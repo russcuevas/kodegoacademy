3a8db1c62c6595d1d5676296820a62c9d612c1dd
russcuevas <russelcuevas0@gmail.com>
2023-12-07 13:43:23 +0800

    hello

--- resources/views/admin/Layout/_Footer.blade.php
+++ resources/views/admin/Layout/_Footer.blade.php
@@ -1,4 +1,4 @@
-    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+    <script src="https://code.jquery.com/jquery-3.4.0.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap/dist/js/bootstrap.bundle.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
     <script src="https://cdn.datatables.net/1.13.6/js/jquery.dataTables.min.js"></script>
